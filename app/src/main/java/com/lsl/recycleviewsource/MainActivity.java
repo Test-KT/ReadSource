be5cf8ee54213ae7a04d144bbb0b7df2de5a8809
lsl <408930131@qq.com>
2018-04-08 18:00:24 +0800

    update

--- app/src/main/java/com/lsl/recycleviewsource/MainActivity.java
+++ app/src/main/java/com/lsl/recycleviewsource/MainActivity.java
@@ -2,12 +2,46 @@ package com.lsl.recycleviewsource;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
 
 public class MainActivity extends AppCompatActivity {
+    RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recycle);
+
+        //set layoutmanager
+        LinearLayoutManager linearLayoutManager=new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(linearLayoutManager);
+        //set adapter
+        recyclerView.setAdapter(new MyAdapter());
+        //set divider
+        recyclerView.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL));
+    }
+
+
+    class MyAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+        @Override
+        public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            return null;
+        }
+
+        @Override
+        public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return 0;
+        }
     }
 }
---
+++ app/src/main/java/com/lsl/recycleviewsource/TestLayoutManager.java
@@ -0,0 +1,12 @@
+package com.lsl.recycleviewsource;
+
+import android.support.v7.widget.RecyclerView;
+
+public class TestLayoutManager extends RecyclerView.LayoutManager {
+    @Override
+    public RecyclerView.LayoutParams generateDefaultLayoutParams() {
+        return null;
+    }
+
+
+}
